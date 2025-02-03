b4c9381c92f84894ad18a7465dc04d7541a3cc2d
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-02-03 23:58:07 +0500

    psudo for adding task

---
+++ src/todo.js
@@ -0,0 +1,11 @@
+export default function makeTodo () {
+    todosArr = [] 
+
+    const todos = document.getElementById('todos')
+
+    //get todo data with form
+
+    //push form data to todo arr
+
+    //send todoarr to the dom func
+}
