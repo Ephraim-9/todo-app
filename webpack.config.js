0db475771c4d61ffe9990c61de72f5e6ccf65dfb
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-02-03 19:30:35 +0500

    Initial project setup with Webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+// webpack.config.js
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Todo App',
+      template: 'index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    hot: true,
+    port: 3000,
+    open: true
+  }
+};
