5167b8814a46541dc09d3dd8485f68e124dc97ed
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-02-06 12:01:32 +0500

    fix dynamic loading of stored todos

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { getFormData } from './dom';
+import { renderStoredTodos } from './storage';
 import './styles.css';
 
 getFormData();
+renderStoredTodos();
--- src/storage.js
+++ src/storage.js
@@ -1,12 +1,15 @@
 import { renderTodo } from './dom';
 
-const todosArr = [];
-
 export function storeTodo(todo) {
+  const todosArr = JSON.parse(localStorage.getItem('todos')) || [];
   todosArr.push(todo);
 
   localStorage.setItem('todos', JSON.stringify(todosArr));
 
+  renderStoredTodos();
+}
+
+export function renderStoredTodos() {
   const sotoredTodos = localStorage.getItem('todos');
   const sotoredTodosArr = JSON.parse(sotoredTodos);
 
