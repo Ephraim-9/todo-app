7a173aed2cf3c6dbc9fcbcfecc0f9b5cc49067f4
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-02-06 17:30:03 +0500

    improve todo function

--- src/todo.js
+++ src/todo.js
@@ -1,5 +1,18 @@
 import { storeTodo } from './storage';
 
-export default function makeTodo(sentTodo) {
-  storeTodo(sentTodo);
+export default function makeTodo(name, date, desc) {
+  const newTodo = {
+    name,
+    date,
+    desc,
+  };
+
+  madeTodo(newTodo);
+
+  return newTodo;
+}
+
+function madeTodo(todo) {
+  storeTodo(todo);
+  console.log(todo);
 }
