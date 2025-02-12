ae4d0d4819720996cc98d725d42591de0a5a5ea0
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-02-12 19:47:01 +0500

    add project

--- src/todo.js
+++ src/todo.js
@@ -1,18 +1,14 @@
 import { storeTodo } from './storage';
 
-export default function makeTodo(name, date, desc) {
+export default function makeTodo(name, date, desc, projectName) {
   const newTodo = {
     name,
     date,
     desc,
+    project: projectName,
   };
 
-  madeTodo(newTodo);
+  storeTodo(newTodo);
 
   return newTodo;
-}
-
-function madeTodo(todo) {
-  storeTodo(todo);
-  console.log(todo);
 }
